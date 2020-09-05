293cc7bef0240f9c6bda76f8f729161afafe7f1d
Priyank Mithaiwala <priyankmithaiwala@gmail.com>
2020-09-05 13:44:00 +0530

    ch4 completed

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,6 +1,8 @@
-import 'package:awsomeapp/ChangeNameCard.dart';
+// import 'package:awsomeapp/ChangeNameCard.dart';
 import 'package:awsomeapp/Drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,25 +12,49 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change ME";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
+    // print(res.body);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                backgroundColor: Colors.white,
+              )),
       ),
       drawer: MyDrawer(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
@@ -43,3 +69,4 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+// child: ChangeNameCard(myText: myText, nameController: _nameController),
